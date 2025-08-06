427b1c67d08dd1ebf7e10c5fc473fca0fc5bc587
gabrieltoadere <gabriel.toadere.2024@mumail.ie>
2025-08-06 15:46:54 +0100

    finished main page anims

--- anim.js
+++ anim.js
@@ -32,12 +32,58 @@ window.addEventListener('scroll',function () {
 })
 
 window.addEventListener('scroll',function () {
-    var element = this.document.querySelectorAll('.review-anim')
+    var element = this.document.querySelector('.reviews')
     var position = element.getBoundingClientRect();
 
     if(position.top < this.window.innerHeight && position.bottom >=0) {
-        element.opacity=1;
+        element.classList.add('visible')
     } else {
-        element.opacity=0;
+        element.classList.remove('visible')
     }
 })
+
+window.addEventListener('scroll',function () {
+    var element = this.document.querySelector('.review-header')
+    var position = element.getBoundingClientRect();
+
+    if(position.top < this.window.innerHeight && position.bottom >=0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll',function () {
+    var element = this.document.querySelector('.contact-button')
+    var position = element.getBoundingClientRect();
+
+    if(position.top < this.window.innerHeight && position.bottom >=0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector(".branding-header");
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            header.classList.add("animate-border");
+            observer.unobserve(header);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (header) {
+      observer.observe(header);
+    }
+  });
