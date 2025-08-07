fb72ec3b63952da0e6aae77ea24006d1d83fba99
gabrieltoadere <gabriel.toadere.2024@mumail.ie>
2025-08-07 21:45:14 +0100

    about-us finished

--- anim.js
+++ anim.js
@@ -35,10 +35,16 @@ window.addEventListener('scroll',function () {
     var element = this.document.querySelector('.reviews')
     var position = element.getBoundingClientRect();
 
-    if(position.top < this.window.innerHeight && position.bottom >=0) {
-        element.classList.add('visible')
+    var elementHeight = position.height || element.offsetHeight;
+
+    var visibleTop = Math.max(position.top, 0);
+    var visibleBottom = Math.min(position.bottom, window.innerHeight);
+    var visibleHeight = visibleBottom - visibleTop;
+
+    if (visibleHeight >= elementHeight / 2) {
+        element.classList.add('visible');
     } else {
-        element.classList.remove('visible')
+        element.classList.remove('visible');
     }
 })
 
@@ -63,7 +69,6 @@ window.addEventListener('scroll',function () {
         element.classList.remove('visible')
     }
 })
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -94,3 +99,12 @@ window.addEventListener("load", function () {
     document.querySelector(".header-buttons-container").classList.add("fade-in");
     document.querySelector(".welcome-text-container").classList.add("fade-in");
 });
+
+window.addEventListener('load', () => {
+    const inners = document.querySelectorAll('.slide-up-inner');
+    inners.forEach((inner, index) => {
+      setTimeout(() => {
+        inner.classList.add('visible');
+      }, index * 100);
+    });
+  });
