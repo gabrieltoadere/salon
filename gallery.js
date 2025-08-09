0485248e4058d101532a165d6243da2ce3d776c5
gabrieltoadere <gabriel.toadere.2024@mumail.ie>
2025-08-10 00:32:21 +0100

    gallery start

--- gallery.js
+++ gallery.js
@@ -1,23 +1,27 @@
+const track = document.getElementById('image-track');
+
 window.onmousedown = e => {
   track.dataset.mouseDownAt=e.clientX;
 }
 
 window.onmouseup = e => {
   track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentge = track.dataset.percentage;
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 window.onmousemove = e => {
 
   if(track.dataset.mouseDownAt === "0") return;
 
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,maxDelta = window.innerWidth / 2;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
 
-
-  const percentage = (mouseDelta / maxDelta) * -100,nextPercentage = parseFloat(track.dataset.prevPercentge) + percentage;
-
+  const percentage = (mouseDelta / maxDelta) * -100,
+  nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
   track.dataset.percentage = nextPercentage;
 
   track.style.transform = `translate(${nextPercentage}%,-50%)`;
 }
+
+
